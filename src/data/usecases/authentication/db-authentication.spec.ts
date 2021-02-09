fb998321136197d1ed029b1136ff8489e948a019
Edson Shideki Kokado <eskokado@gmail.com>
2021-02-09 09:31:56 -0300

    refactor: change models to use type alias instead of interfaces

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -52,7 +52,7 @@ const makeUpdateAccessTokenRepository = (): UpdateAccessTokenRepository => {
   return new UpdateAccessTokenRepositoryStub()
 }
 
-interface SutTypes {
+type SutTypes = {
   sut: DbAuthentication
   loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository
   hashComparerStub: HashComparer
--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
@@ -29,7 +29,7 @@ const makeFakeSurveys = (): SurveyModel[] => {
   ]
 }
 
-interface SutTypes {
+type SutTypes = {
   sut: LoadSurveysController
   loadSurveysStub: LoadSurveys
 }
