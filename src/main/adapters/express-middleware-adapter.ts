78fe3217ae113151eb6aac5fff0468b99eaa4b6b
Edson Shideki Kokado <eskokado@gmail.com>
2021-02-11 15:42:37 -0300

    feat: ensure save survey result route returns 200 on success

--- src/main/adapters/express-middleware-adapter.ts
+++ src/main/adapters/express-middleware-adapter.ts
@@ -4,7 +4,8 @@ import { Middleware, HttpRequest } from '@/presentation/protocols'
 export const adaptMiddleware = (middleware: Middleware) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     const httpRequest: HttpRequest = {
-      headers: req.headers
+      headers: req.headers,
+      accountId: req.accountId
     }
     const httpResponse = await middleware.handle(httpRequest)
     if (httpResponse.statusCode === 200) {
