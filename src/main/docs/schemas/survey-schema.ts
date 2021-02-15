ef421522b40c46a013d0619cfc4b6d6eb5a6effd
Edson Shideki Kokado <eskokado@gmail.com>
2021-02-15 06:35:36 -0300

    docs: add load survey api docs

---
+++ src/main/docs/schemas/survey-schema.ts
@@ -0,0 +1,21 @@
+export const surveySchema = {
+  type: 'object',
+  properties: {
+    id: {
+      type: 'string'
+    },
+    question: {
+      type: 'string'
+    },
+    answer: {
+      type: 'array',
+      items: {
+        $ref: '#/schemas/surveyAnswer'
+      }
+    },
+    date: {
+      type: 'string'
+    }
+  },
+  required: ['id', 'question', 'answers', 'date']
+}
