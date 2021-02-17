3a7895f6130425e17e302af8c8dcec01bc876463
Edson Shideki Kokado <eskokado@gmail.com>
2021-02-17 08:09:44 -0300

    fix: change save survey result to return all answers

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -72,6 +72,8 @@ describe('Survey mongo repository', () => {
       expect(surveyResult.answers[0].answer).toBe(survey.answers[0].answer)
       expect(surveyResult.answers[0].count).toBe(1)
       expect(surveyResult.answers[0].percent).toBe(100)
+      expect(surveyResult.answers[1].count).toBe(0)
+      expect(surveyResult.answers[1].percent).toBe(0)
     })
   })
 
@@ -96,5 +98,7 @@ describe('Survey mongo repository', () => {
     expect(surveyResult.answers[0].answer).toBe(survey.answers[1].answer)
     expect(surveyResult.answers[0].count).toBe(1)
     expect(surveyResult.answers[0].percent).toBe(100)
+    expect(surveyResult.answers[1].count).toBe(0)
+    expect(surveyResult.answers[1].percent).toBe(0)
   })
 })
