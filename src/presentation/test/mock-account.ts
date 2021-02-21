10b05177ec4104e562db59f10e335a0b718091f6
Edson Shideki Kokado <eskokado@gmail.com>
2021-02-21 08:56:52 -0300

    test: add name to authentication response

--- src/presentation/test/mock-account.ts
+++ src/presentation/test/mock-account.ts
@@ -1,3 +1,4 @@
+import { AuthenticationModel } from '@/domain/models/authentication'
 import { AddAccount, AddAccountParams } from '@/domain/usecases/account/add-account'
 import { Authentication, AuthenticationParams } from '@/domain/usecases/account/authentication'
 import { LoadAccountByToken } from '@/domain/usecases/account/load-account-by-token'
@@ -17,11 +18,14 @@ export class AddAccountSpy implements AddAccount {
 
 export class AuthenticationSpy implements Authentication {
   authenticationParams: AuthenticationParams
-  token = faker.random.uuid()
+  authenticationModel = {
+    accessToken: faker.random.uuid(),
+    name: faker.name.findName()
+  }
 
-  async auth (authenticationParams: AuthenticationParams): Promise<string> {
+  async auth (authenticationParams: AuthenticationParams): Promise<AuthenticationModel> {
     this.authenticationParams = authenticationParams
-    return Promise.resolve(this.token)
+    return this.authenticationModel
   }
 }
 
@@ -33,6 +37,6 @@ export class LoadAccountByTokenSpy implements LoadAccountByToken {
   async load (accessToken: string, role?: string): Promise<AccountModel> {
     this.accessToken = accessToken
     this.role = role
-    return Promise.resolve(this.accountModel)
+    return this.accountModel
   }
 }
