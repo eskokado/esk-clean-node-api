8da75eb1ef8726b4cdcc2e1c5c2b94ed3fa860a7
Edson Shideki Kokado <eskokado@gmail.com>
2021-02-26 05:49:49 -0300

    refactor: add types to AddAccount usecase

--- tests/data/mocks/mock-db-account.ts
+++ tests/data/mocks/mock-db-account.ts
@@ -1,13 +1,12 @@
-import { AddAccountParams } from '@/domain/usecases'
 import { AccountModel } from '@/domain/models'
 import { mockAccountModel } from '@/tests/domain/mocks'
 import { AddAccountRepository, LoadAccountByTokenRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository } from '@/data/protocols/db'
 
 export class AddAccountRepositorySpy implements AddAccountRepository {
   accountModel = mockAccountModel()
-  addAccountParams: AddAccountParams
+  addAccountParams: AddAccountRepository.Params
 
-  async add (data: AddAccountParams): Promise<AccountModel> {
+  async add (data: AddAccountRepository.Params): Promise<AccountModel> {
     this.addAccountParams = data
     return Promise.resolve(this.accountModel)
   }
