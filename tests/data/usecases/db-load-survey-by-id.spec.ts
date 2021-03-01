38de6ca7e6fb1fb4c44f6a60ddf7ebfa8e16a23b
Edson Shideki Kokado <eskokado@gmail.com>
2021-03-01 10:13:28 -0300

    refactor: add types to LoadSurveyById usecase

--- tests/data/usecases/db-load-survey-by-id.spec.ts
+++ tests/data/usecases/db-load-survey-by-id.spec.ts
@@ -42,7 +42,7 @@ describe('DbLoadSurveyById UseCase', () => {
   test('Should return Survey on success', async () => {
     const { sut, loadSurveyByIdRepositorySpy } = makeSut()
     const survey = await sut.loadById(surveyId)
-    expect(survey).toEqual(loadSurveyByIdRepositorySpy.surveyModel)
+    expect(survey).toEqual(loadSurveyByIdRepositorySpy.result)
   })
 
   test('Should throw if LoadSurveyByIdRepository throws', async () => {
